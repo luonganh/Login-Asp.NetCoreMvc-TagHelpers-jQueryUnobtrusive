0f182ba7a8a7ca9c1fe430db954914e0fa69c5ba
LuongAnh <luonganh@gmail.com>
2024-03-01 21:33:14 +0700

    Add Data annotations, custom validation attributes to model.

--- Asp.NetCore.Services/Models/Identity/LoginViewModel.cs
+++ Asp.NetCore.Services/Models/Identity/LoginViewModel.cs
@@ -1,9 +1,25 @@
-﻿namespace Asp.NetCore.Services.Models.Identity
+﻿using Asp.NetCore.Services.Attributes;
+using System.ComponentModel.DataAnnotations;
+using RangeAttribute = Asp.NetCore.Services.Attributes.RangeAttribute;
+
+namespace Asp.NetCore.Services.Models.Identity
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Please enter the username")]
+        [Range(4, 50)]
+        [NoWhiteSpaces]
+        [StartWithLetter]
+        [EnglishChars]
         public string? Username { get; set; }
 
+        [Required(ErrorMessage = "Please enter the password")]
+        [Range(6, 30)]
+        [NoWhiteSpaces]
+        [StartWithLetter]
+        [AtLeastOneNumber]
+        [AtLeastOneSpecialCharacter]
+        [AtLeastOneUppercaseLetterOneLowercaseLetter]
         public string? Password { get; set; }
 
         public bool RememberMe { get; set; } = false;
